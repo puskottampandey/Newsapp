696363eaa731c2344e65d4bfa8b86e4348ecd16a
puskottampandey <puskottam.pandey@gmail.com>
2023-02-23 22:53:35 +0545

    fetched news

--- lib/homepage_screen.dart
+++ lib/homepage_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:newsapp/constant/url_constant.dart';
+import 'package:newsapp/widget/news_card.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -23,53 +24,70 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                color: Colors.white,
+                onPressed: () {
+                  exit(0);
+                },
+                icon: const Icon(Icons.logout))
+          ],
+          centerTitle: true,
+          backgroundColor: Colors.red,
+          title: const Text(
+            "NewsApp",
+            style: TextStyle(color: Colors.white),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: FutureBuilder(
+              future: fetchNews(),
+              builder: ((context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  return Column(children: [
+                    ...snapshot.data["articles"].map((e) => Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(12)),
+                            child: Column(
+                              children: [
+                                Image.network(e["urlToImage"] ?? No_image),
+                                const Padding(padding: EdgeInsets.all(8)),
+                                Column(
+                                  children: [
+                                    Text(
+                                      e["title"] ?? "...",
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    e["description"] ?? "..",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        )),
+                  ]);
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.red,
+                    ),
+                  );
+                }
+              }),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-          )
-        ],
-      ),
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              color: Colors.white,
-              onPressed: () {
-                exit(0);
-              },
-              icon: const Icon(Icons.logout))
-        ],
-        centerTitle: true,
-        backgroundColor: Colors.red,
-        title: const Text(
-          "NewsApp",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: Center(
-        child: FutureBuilder(
-          future: fetchNews(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Text(snapshot.data.toString()),
-                  ],
-                ),
-              );
-            } else {
-              return const CircularProgressIndicator();
-            }
-          },
-        ),
-      ),
-    );
+        ));
   }
 }
---
+++ lib/widget/news_card.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/constant/url_constant.dart';
+
+//getNewsCard(e) {
+ // return Image.network(e["urlToImage "]);
+//}
